762fc04e733980ee986e1debe425f5a3a26a330e
ERP <erp@ITD-500.local>
2018-10-10 11:53:08 +0400

    initial commit

---
+++ src/main/java/io/javabrains/springBootStarter/course/CourseService.java
@@ -0,0 +1,43 @@
+package io.javabrains.springBootStarter.course;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseService {
+
+	@Autowired
+	private CourseRepository CourseRepository;
+
+	public List<Course> getAllCourses(String topicId) {
+
+		List<Course> courses = new ArrayList<>();
+		CourseRepository.findByTopicId(topicId)
+		.forEach(courses::add);
+		return courses;
+	}
+
+	public Course getCourse(String id) {
+		return CourseRepository.findById(id).orElse(null);
+	}
+
+	public void addCourse(Course course) {
+
+		CourseRepository.save(course);
+	}
+
+	public void updateCourse(Course course) {
+		CourseRepository.save(course);
+
+	}
+
+	public void deleteCourse(String id) {
+
+		CourseRepository.deleteById(id);
+	}
+
+}
